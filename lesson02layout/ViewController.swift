6ca6b4800513e00ba3cfa577a9f81d2dc86933b9
C Daly <colin.p.s.daly@gmail.com>
2020-06-25 15:08:57 +0100

    Initial Commit

---
+++ lesson02layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson02layout
+//
+//  Created by Colin Daly on 25/06/2020.
+//  Copyright © 2020 Colin Daly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
